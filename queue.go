c5d5dd9ee9c47811e1fc3ababbb0f7546d937729
Martin Czygan <martin.czygan@gmail.com>
2015-11-14 20:08:42 +0100

    simplify queue for the non-concurrent use case

--- queue.go
+++ queue.go
@@ -1,40 +1,28 @@
 package xmlcutty
 
-import (
-	"strings"
-	"sync"
-)
+import "strings"
 
 type StringFifo struct {
-	mu    sync.RWMutex
 	queue []string
 }
 
 func (q *StringFifo) Push(s string) {
-	q.mu.Lock()
-	defer q.mu.Unlock()
 	q.queue = append(q.queue, s)
 }
 
 func (q *StringFifo) Top() string {
 	if len(q.queue) == 0 {
-		panic("pop from empty queue")
+		panic("Top from empty queue")
 	}
 	return q.queue[len(q.queue)-1]
 }
 
 func (q *StringFifo) Pop() string {
 	if len(q.queue) == 0 {
-		panic("pop from empty queue")
-	}
-	var u []string
-	for _, s := range q.queue[:len(q.queue)-1] {
-		u = append(u, s)
+		panic("Pop from empty queue")
 	}
 	r := q.Top()
-	q.mu.Lock()
-	defer q.mu.Unlock()
-	q.queue = u
+	q.queue = q.queue[:len(q.queue)-1]
 	return r
 }
 
