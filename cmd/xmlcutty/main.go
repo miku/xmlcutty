7ccf9a2399bf98c1c9dff867711f08d51ba159b8
Martin Czygan <martin.czygan@gmail.com>
2015-11-23 13:43:40 +0100

    docs and license update

--- cmd/xmlcutty/main.go
+++ cmd/xmlcutty/main.go
@@ -1,4 +1,28 @@
-// The game ain't in me no more.
+// Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+//                   The Finc Authors, http://finc.info
+//                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+//
+// This file is part of some open source application.
+//
+// Some open source application is free software: you can redistribute
+// it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// Some open source application is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+//
+//
+// xmlcutty is a simple tool for carving out elements from large XML files,
+// fast. Since it works in a streaming fashion, it uses almost no memory and
+// can process around 1G of XML per minute.
 package main
 
 import (
@@ -14,12 +38,14 @@ import (
 	"github.com/miku/xmlcutty"
 )
 
+// Version of xmlcutty.
 const Version = "0.1.3"
 
-type Dummy struct {
+type dummy struct {
 	Text []byte `xml:",innerxml"`
 }
 
+// lastElement returns the last element of a path like string.
 func lastElement(p string) string {
 	parts := strings.Split(p, "/")
 	if len(parts) == 0 {
@@ -82,6 +108,7 @@ func main() {
 	if *root != "" {
 		fmt.Println("<" + *root + ">")
 	}
+
 	for {
 		t, err := decoder.Token()
 		if err == io.EOF {
@@ -94,20 +121,20 @@ func main() {
 		case xml.StartElement:
 			stack.Push(e.Name.Local)
 			if *path == stack.String() {
-				var dummy Dummy
-				err := decoder.DecodeElement(&dummy, &e)
-				if err != nil {
+				var d dummy
+				if err := decoder.DecodeElement(&d, &e); err != nil {
 					log.Fatal(err)
 				}
 				stack.Pop()
 				fmt.Print(opener)
-				fmt.Print(string(dummy.Text))
+				fmt.Print(string(d.Text))
 				fmt.Print(closer)
 			}
 		case xml.EndElement:
 			stack.Pop()
 		}
 	}
+
 	if *root != "" {
 		fmt.Println("</" + *root + ">")
 	}
--- stack.go
+++ stack.go
@@ -1,15 +1,41 @@
+// Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+//                   The Finc Authors, http://finc.info
+//                   Martin Czygan, <martin.czygan@uni-leipzig.de>
+//
+// This file is part of some open source application.
+//
+// Some open source application is free software: you can redistribute
+// it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// Some open source application is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+//
+//
+// package xmlcutty implements support for the xmlcutty command line tool.
 package xmlcutty
 
 import "strings"
 
+// StringStack implements LIFO. Not thread safe.
 type StringStack struct {
 	queue []string
 }
 
+// Push adds an element to the stack.
 func (q *StringStack) Push(s string) {
 	q.queue = append(q.queue, s)
 }
 
+// Top retrieves the last added element. Panics on an empty stack.
 func (q *StringStack) Top() string {
 	if len(q.queue) == 0 {
 		panic("Top from empty queue")
@@ -17,6 +43,8 @@ func (q *StringStack) Top() string {
 	return q.queue[len(q.queue)-1]
 }
 
+// Pop removes the last added element from the stack and returns it. Panics on
+// an empty stack.
 func (q *StringStack) Pop() string {
 	if len(q.queue) == 0 {
 		panic("Pop from empty queue")
@@ -26,6 +54,7 @@ func (q *StringStack) Pop() string {
 	return r
 }
 
+// String formats the stack in a path-like manner.
 func (q *StringStack) String() string {
 	return "/" + strings.Join(q.queue, "/")
 }
